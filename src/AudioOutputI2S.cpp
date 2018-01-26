a7d6f4d76c64efad7d87b2bbdf4e44755436870a
h3ndrik <hendrik+dev@xd0.de>
2018-01-26 16:02:09 +0100

    increase dma buffer

--- src/AudioOutputI2S.cpp
+++ src/AudioOutputI2S.cpp
@@ -46,7 +46,7 @@ AudioOutputI2S::AudioOutputI2S(int port, bool builtInDAC)
       .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
       .communication_format = (i2s_comm_format_t)((builtInDAC ? 0 : I2S_COMM_FORMAT_I2S) | I2S_COMM_FORMAT_I2S_MSB),
       .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
-      .dma_buf_count = 8,
+      .dma_buf_count = 32,
       .dma_buf_len = 64,   //Interrupt level 1
       .use_apll = 0 // Use audio PLL
     };
